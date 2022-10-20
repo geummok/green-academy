428a6551c0112fcf0e40d1370fca7d740dcb4d65
geummok <115681671+geummok@users.noreply.github.com>
2022-10-20 21:49:11 +0900

    Add files via upload

---
+++ hd_jquery-main/js/main.js
@@ -0,0 +1,23 @@
+$(function () {
+    $('.top_banner_slide').slick({
+        arrows: false,
+        dots: true,
+    });
+
+    $('.top_banner i').on('click', function () {
+        $('.top_banner').slideUp(200)
+    });
+
+    $(window).on('scroll', function() {
+        var sct = $(window).scrollTop()
+        if(sct > 0) {
+            $('.header .bottom').addClass('on')
+        } else {
+            $('.header .bottom.').removeClass('on')
+        }
+    })
+})
+
+$('.header .xi-global').on('click', function() {
+    $('header .global').toggleClass('on')
+})
